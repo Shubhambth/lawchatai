92cbca7213f7d3ec6d7a12e02220540f54c32171
Shubham Singh <164128999+Shubhambth@users.noreply.github.com>
2025-03-21 11:50:19 +0000

    v5

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def home(request):
-  posts = Post.objects.all()
-  
-  return render(request,'home.html',{"posts":posts})
+  posts = Post.objects.all().order_by('-created_at')
+  paginator = Paginator(posts, 6)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+  return render(request,'home.html',{'page_obj': page_obj})
 
 
 
